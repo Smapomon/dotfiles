f2c240b8f4591bf65f2c2bdde9affc7f3ec369d3
Smapomon <sahlaster95@gmail.com>
2025-05-17 13:44:22 +0300

    cleanup

--- .config/awesome/themes/dremora/theme.lua
+++ .config/awesome/themes/dremora/theme.lua
@@ -66,13 +66,7 @@ theme.awesome_icon                              = theme.dir .."/icons/awesome.pn
 theme.taglist_squares_sel                       = theme.dir .. "/icons/square_sel.png"
 theme.taglist_squares_unsel                     = theme.dir .. "/icons/square_unsel.png"
 theme.layout_tile                               = theme.dir .. "/icons/tile.png"
-theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
-theme.layout_tilebottom                         = theme.dir .. "/icons/tilebottom.png"
-theme.layout_tiletop                            = theme.dir .. "/icons/tiletop.png"
-theme.layout_fairv                              = theme.dir .. "/icons/fairv.png"
-theme.layout_fairh                              = theme.dir .. "/icons/fairh.png"
 theme.layout_spiral                             = theme.dir .. "/icons/spiral.png"
-theme.layout_dwindle                            = theme.dir .. "/icons/dwindle.png"
 theme.layout_max                                = theme.dir .. "/icons/max.png"
 theme.layout_fullscreen                         = theme.dir .. "/icons/fullscreen.png"
 theme.layout_magnifier                          = theme.dir .. "/icons/magnifier.png"
@@ -108,91 +102,42 @@ local gray       = "#858585"
 
 -- Textclock
 local mytextclock = wibox.widget.textclock(
-    markup(gray, " | ") .. markup(white, " %d.") .. markup(white, "%m.") .. markup(white, "%Y - ") ..  markup(white, "%H:%M:%S ")
+  markup(gray, " | ") .. markup(white, " %d.") .. markup(white, "%m.") .. markup(white, "%Y - ") ..  markup(white, "%H:%M:%S ")
 )
 mytextclock.font    = theme.font
 mytextclock.refresh = 1
 
 -- Calendar
 theme.cal = lain.widget.cal({
-    -- calendar props
-    followtag           = true,
-    week_start          = 2,
-    week_number         = "left",
-    three               = true,
-    attach_to           = { mytextclock },
-    notification_preset = {
-        font            = "Terminus 11",
-        fg              = white,
-        bg              = theme.bg_normal
-}})
-
--- Mail IMAP check
---[[ to be set before use
-theme.mail = lain.widget.imap({
-    timeout  = 180,
-    server   = "server",
-    mail     = "mail",
-    password = "keyring get mail",
-    notification_preset = { fg = white }
-    settings = function()
-        mail  = ""
-        count = ""
-
-        if mailcount > 0 then
-            mail = "Mail "
-            count = mailcount .. " "
-        end
-
-        widget:set_markup(markup.font(theme.font, markup(gray, mail) .. markup(white, count)))
-    end
-})
---]]
+  -- calendar props
+  followtag           = true,
+  week_start          = 2,
+  week_number         = "left",
+  three               = true,
+  attach_to           = { mytextclock },
+  notification_preset = {
+    font            = "Terminus 11",
+    fg              = white,
+    bg              = theme.bg_normal
+  }})
 
 -- MPD
 theme.mpd = lain.widget.mpd({
-    settings = function()
-        mpd_notification_preset.fg = white
-        artist = mpd_now.artist .. " "
-        title  = mpd_now.title  .. " "
+  settings = function()
+    mpd_notification_preset.fg = white
+    artist = mpd_now.artist .. " "
+    title  = mpd_now.title  .. " "
 
-        if mpd_now.state == "pause" then
-            artist = "mpd "
-            title  = "paused "
-        elseif mpd_now.state == "stop" then
-            artist = ""
-            title  = ""
-        end
-
-        widget:set_markup(markup.font(theme.font, markup(gray, artist) .. markup(white, title)))
+    if mpd_now.state == "pause" then
+      artist = "mpd "
+      title  = "paused "
+    elseif mpd_now.state == "stop" then
+      artist = ""
+      title  = ""
     end
-})
 
--- /home fs
---[[ commented because it needs Gio/Glib >= 2.54
-theme.fs = lain.widget.fs({
-    notification_preset = { fg = white, bg = theme.bg_normal, font = "Terminus 10.5" },
-    settings  = function()
-        fs_header = ""
-        fs_p      = ""
-
-        if fs_now["/home"].percentage >= 90 then
-            fs_header = " Hdd "
-            fs_p      = fs_now["/home"].percentage
-        end
-
-        widget:set_markup(markup.font(theme.font, markup(gray, fs_header) .. markup(white, fs_p)))
-    end
-})
---]]
-
--- Battery
-local bat = lain.widget.bat({
-    settings = function()
-        bat_header = " Bat "
-        bat_p      = bat_now.perc .. " "
-        widget:set_markup(markup.font(theme.font, markup(gray, bat_header) .. markup(white, bat_p)))
-    end
+    widget:set_markup(markup.font(theme.font, markup(gray, artist) .. markup(white, title)))
+  end
 })
 
 -- Last active app
@@ -214,61 +159,58 @@ end
 
 -- ALSA volume
 --local volume_widget = wibox.container.background(volume.bar)
-
 volume_widget = wibox.widget.textbox()
 function update_volume(widget)
-    -- Get volume for master with amixer
-    local status = ''
-    local volstring = "NO VOLUME DEVICE "
-    awful.spawn.easy_async("amixer sget Master", function(stdout)
-        status = stdout
+  -- Get volume for master with amixer
+  local status = ''
+  local volstring = "NO VOLUME DEVICE "
+  awful.spawn.easy_async("amixer sget Master", function(stdout)
+    status = stdout
 
-        -- Parse volume
-        local vol_num = tonumber(string.match(status, "(%d?%d?%d)%%"))
+    -- Parse volume
+    local vol_num = tonumber(string.match(status, "(%d?%d?%d)%%"))
 
-        -- Assign incase io stream is not read correctly for example
-        if vol_num == nil then vol_num = 0 end
+    -- Assign incase io stream is not read correctly for example
+    if vol_num == nil then vol_num = 0 end
 
-        local volume  = vol_num / 10 -- Might be a fukkuppy with number converion but vol is multiplied by 10
-        status        = string.match(status, "%[(o[^%]]*)%]")
+    local volume  = vol_num / 10 -- Might be a fukkuppy with number converion but vol is multiplied by 10
+    status        = string.match(status, "%[(o[^%]]*)%]")
 
-        -- Set volume to widget
-        volstring = "Vol: "
-        if type(status) == "string" and string.find(status, "on", 1, true)
-        then
-            local volint = math.floor(volume * 10)
+    -- Set volume to widget
+    volstring = "Vol: "
+    if type(status) == "string" and string.find(status, "on", 1, true)
+    then
+      local volint = math.floor(volume * 10)
 
-            if volint > 0
-            then
-                volstring   = volstring .. volint .. "%"
-            else
-                volstring = volstring .. "muted"
-            end
-        else
-            volstring = "NO VOLUME DEVICE"
-        end
+      if volint > 0
+      then
+        volstring   = volstring .. volint .. "%"
+      else
+        volstring = volstring .. "muted"
+      end
+    else
+      volstring = "NO VOLUME DEVICE"
+    end
 
-        widget:set_text(volstring)
-    end)
+    widget:set_text(volstring)
+  end)
 end
 
-
 theme.volume = lain.widget.alsa({
-    --togglechannel = "IEC958,3",
-    settings = function()
-        header = " Vol "
-        vlevel  = volume_now.level
+  --togglechannel = "IEC958,3",
+  settings = function()
+    header = " Vol "
+    vlevel  = volume_now.level
 
-        if volume_now.status == "off" then
-            vlevel = vlevel .. "M "
-        else
-            vlevel = vlevel .. " "
-        end
-
-        widget:set_markup(markup.font(theme.font, markup(gray, header) .. markup(white, vlevel)))
+    if volume_now.status == "off" then
+      vlevel = vlevel .. "M "
+    else
+      vlevel = vlevel .. " "
     end
-})
 
+    widget:set_markup(markup.font(theme.font, markup(gray, header) .. markup(white, vlevel)))
+  end
+})
 
 paru_updates_widget = wibox.widget.textbox()
 LAST_UPDATE_TIME    = os.time()
@@ -323,10 +265,10 @@ end
 update_package_count()
 
 gears.timer {
-    timeout = 10, -- seconds
-    autostart = true,
-    call_now = true,
-    callback = update_package_count
+  timeout = 10, -- seconds
+  autostart = true,
+  call_now = true,
+  callback = update_package_count
 }
 
 -- Separators
@@ -335,174 +277,180 @@ local arrl_pre  = separators.arrow_right("alpha", "#1A1A1A")
 local arrl_post = separators.arrow_right("#1A1A1A", "alpha")
 
 function theme.at_screen_connect(s)
-    -- Quake application
-    s.quake = lain.util.quake({ app = awful.util.terminal })
+  -- Quake application
+  s.quake = lain.util.quake({ app = awful.util.terminal })
 
-    -- If wallpaper is a function, call it with the screen
-    local wallpaper = theme.wallpaper
-    if type(wallpaper) == "function" then
-        wallpaper = wallpaper(s)
-    end
-    gears.wallpaper.maximized(wallpaper, s, true)
+  -- If wallpaper is a function, call it with the screen
+  local wallpaper = theme.wallpaper
+  if type(wallpaper) == "function" then
+    wallpaper = wallpaper(s)
+  end
+  gears.wallpaper.maximized(wallpaper, s, true)
 
-    -- Tags
-    local custom_tags    = { "ƀ", "Ƅ", "Ɗ", "ƈ", "ƙ" }
+  -- Tags
+  local custom_tags    = { "ƀ", "Ƅ", "Ɗ", "ƈ", "ƙ" }
 
-    -- LEFT MONITOR
-    if(s.index == monitor_left)
-    then
-        awful.tag.add("MUSIC", {
-            icon               = "/home/smapo/.config/awesome/icons/music.png",
-            layout             = awful.layout.layouts[1],
-            screen             = s,
-            selected           = true
-        })
+  -- LEFT MONITOR
+  if(s.index == monitor_left)
+  then
+    awful.tag.add("MUSIC", {
+      icon               = "/home/smapo/.config/awesome/icons/music.png",
+      layout             = awful.layout.layouts[1],
+      screen             = s,
+      selected           = true
+    })
 
     -- CENTER MONITOR
-    elseif(s.index == monitor_center)
-    then
-        awful.tag.add("MAIN", {
-            icon     = "/home/smapo/.config/awesome/icons/home-icon.png",
-            layout   = awful.layout.layouts[1],
-            screen   = s,
-            selected = true
-        })
+  elseif(s.index == monitor_center)
+  then
+    awful.tag.add("MAIN", {
+      icon     = "/home/smapo/.config/awesome/icons/home-icon.png",
+      layout   = awful.layout.layouts[1],
+      screen   = s,
+      selected = true
+    })
 
-        awful.tag.add("CODE", {
-            icon   = "/home/smapo/.config/awesome/icons/terminal.png",
-            layout = awful.layout.layouts[1],
-            screen = s,
-        })
+    awful.tag.add("CODE", {
+      icon   = "/home/smapo/.config/awesome/icons/terminal.png",
+      layout = awful.layout.layouts[1],
+      screen = s,
+    })
 
-        awful.tag.add("NOTES", {
-            layout = awful.layout.layouts[1],
-            screen = s,
-        })
+    awful.tag.add("NOTES", {
+      layout = awful.layout.layouts[1],
+      screen = s,
+    })
 
     -- RIGHT MONITOR
-    elseif(s.index == monitor_right)
-    then
-        awful.tag.add("WEB & CHAT", {
-            icon   = "/home/smapo/.config/awesome/icons/web-icon.png",
-            layout = awful.layout.layouts[1],
-            screen = s,
-            selected = true
-        })
-    else
-        awful.tag(custom_tags, s, awful.layout.layouts[1])
-    end
+  elseif(s.index == monitor_right)
+  then
+    awful.tag.add("WEB & CHAT", {
+      icon   = "/home/smapo/.config/awesome/icons/web-icon.png",
+      layout = awful.layout.layouts[1],
+      screen = s,
+      selected = true
+    })
+  else
+    awful.tag(custom_tags, s, awful.layout.layouts[1])
+  end
 
 
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(my_table.join(
-                           awful.button({}, 1, function () awful.layout.inc( 1) end),
-                           awful.button({}, 2, function () awful.layout.set( awful.layout.layouts[1] ) end),
-                           awful.button({}, 3, function () awful.layout.inc(-1) end),
-                           awful.button({}, 4, function () awful.layout.inc( 1) end),
-                           awful.button({}, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons)
+  -- Create a promptbox for each screen
+  s.mypromptbox = awful.widget.prompt()
+  -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+  -- We need one layoutbox per screen.
+  s.mylayoutbox = awful.widget.layoutbox(s)
+  s.mylayoutbox:buttons(my_table.join(
+    awful.button({}, 1, function () awful.layout.inc( 1) end),
+    awful.button({}, 2, function () awful.layout.set( awful.layout.layouts[1] ) end),
+    awful.button({}, 3, function () awful.layout.inc(-1) end),
+    awful.button({}, 4, function () awful.layout.inc( 1) end),
+    awful.button({}, 5, function () awful.layout.inc(-1) end)))
+  -- Create a taglist widget
+  s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons)
 
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        -- configs for tasks
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = awful.util.tasklist_buttons,
+  -- Create a tasklist widget
+  s.mytasklist = awful.widget.tasklist {
+    -- configs for tasks
+    screen  = s,
+    filter  = awful.widget.tasklist.filter.currenttags,
+    buttons = awful.util.tasklist_buttons,
 
-        style    = {
-            shape_border_width = 1,
-            shape_border_color = '#777777',
-            shape              = gears.shape.rounded_bar,
+    style    = {
+      shape_border_width = 1,
+      shape_border_color = '#777777',
+      shape              = gears.shape.rounded_bar,
+    },
+
+    layout   = {
+      spacing = 25,
+      spacing_widget = {
+        {
+          forced_width = 5,
+          shape        = gears.shape.circle,
+          widget       = wibox.widget.separator
         },
-
-        layout   = {
-            spacing = 25,
-            spacing_widget = {
-                {
-                    forced_width = 5,
-                    shape        = gears.shape.circle,
-                    widget       = wibox.widget.separator
-                },
-                valign = 'center',
-                halign = 'center',
-                widget = wibox.container.place,
-            },
-            layout  = wibox.layout.fixed.horizontal
-        },
-        -- Notice that there is *NO* wibox.wibox prefix, it is a template,
-        -- not a widget instance.
-        widget_template = {
+        valign = 'center',
+        halign = 'center',
+        widget = wibox.container.place,
+      },
+      layout  = wibox.layout.fixed.horizontal
+    },
+    -- Notice that there is *NO* wibox.wibox prefix, it is a template,
+    -- not a widget instance.
+    widget_template = {
+      {
+        {
+          {
             {
-                {
-                    {
-                        {
-                            id     = 'icon_role',
-                            widget = wibox.widget.imagebox,
-                        },
-                        margins = 2,
-                        widget  = wibox.container.margin,
-                    },
-                    {
-                        id     = 'text_role',
-                        widget = wibox.widget.textbox,
-                    },
-                    layout = wibox.layout.fixed.horizontal,
-                },
-                left   = 10,
-                right  = 10,
-                width  = 300,
-                widget = wibox.container.constraint
+              forced_height = 1,
+              forced_width = 1,
+              {
+                id     = 'clienticon',
+                widget = awful.widget.clienticon,
+              },
+              margins = 5,
+              widget  = wibox.container.margin
             },
-            id           = 'background_role',
-            widget       = wibox.container.background,
+            margins = 2,
+            widget  = wibox.container.margin,
+          },
+          {
+            id     = 'text_role',
+            widget = wibox.widget.textbox,
+          },
+          layout = wibox.layout.fixed.horizontal,
         },
-    }
+        left   = 10,
+        right  = 10,
+        width  = 300,
+        widget = wibox.container.constraint
+      },
+      id           = 'background_role',
+      widget       = wibox.container.background,
+    },
+  }
 
-    -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s, height = dpi(18), bg = theme.bg_normal, fg = theme.fg_normal })
+  -- Create the wibox
+  s.mywibox = awful.wibar({ position = "top", screen = s, height = dpi(18), bg = theme.bg_normal, fg = theme.fg_normal })
 
-    local updates_widget = nil
-    if(s.index == monitor_center) then
-      updates_widget = paru_updates_widget
-    end
+  local updates_widget = nil
+  if(s.index == monitor_center) then
+    updates_widget = paru_updates_widget
+  end
 
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-        expand = "none",
-        layout = wibox.layout.align.horizontal,
-        { -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-            first,
-            s.mytaglist,
-            arrl_pre,
-            s.mylayoutbox,
-            arrl_post,
-            s.mypromptbox,
-            first,
-        },
-        s.mytasklist, -- Middle widget
-        { -- Right widgets
-            layout = wibox.layout.fixed.horizontal,
-            active_app,
-            wibox.widget.systray(),
-            first,
-            volume_widget,
-            updates_widget,
-            mytextclock,
-        },
-    }
+  -- Add widgets to the wibox
+  s.mywibox:setup {
+    expand = "none",
+    layout = wibox.layout.align.horizontal,
+    { -- Left widgets
+      layout = wibox.layout.fixed.horizontal,
+      first,
+      s.mytaglist,
+      arrl_pre,
+      s.mylayoutbox,
+      arrl_post,
+      s.mypromptbox,
+      first,
+    },
+    s.mytasklist, -- Middle widget
+    { -- Right widgets
+      layout = wibox.layout.fixed.horizontal,
+      active_app,
+      wibox.widget.systray(),
+      first,
+      volume_widget,
+      updates_widget,
+      mytextclock,
+    },
+  }
 
-        -- Force widgets to main monitor
-    if(s.index == monitor_center)
-    then
-        local tray = wibox.widget.systray()
-        tray:set_screen(s)
-    end
+  -- Force widgets to main monitor
+  if(s.index == monitor_center)
+  then
+    local tray = wibox.widget.systray()
+    tray:set_screen(s)
+  end
 
 end
 
